41bea0bed0c4c61f14d920d0a442494c3c68ba25
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-01-05 19:38:38 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("
+const config = require("./config.json");
 
 client.on("message", message => {
   if (message.author.bot) return;
