265d56877252221161e652b005851c5ad1773647
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-01-05 19:38:13 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("
 
-// Set the prefix
-let prefix = "!";
-client.on("message", (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", message => {
+  if (message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
 
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "test")) {
-    message.channel.send("testing!");
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
   }
 });
+
 
 client.login(process.env.BOT_TOKEN);
