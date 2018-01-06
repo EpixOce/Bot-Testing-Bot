3a4f4e3a6b3b3190688d914551a3127bfe020372
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-01-07 10:59:32 +1300

    Update bank.js

--- commands/bank.js
+++ commands/bank.js
@@ -3,11 +3,11 @@ exports.run = (client, message, args) => {
     message.channel.send({embed:{
         color: 3447003,
         author: {
-            name: "Disboard Bank"
+            name: "Lostina Bank"
         },
-        title: "https://docs.google.com/spreadsheets/d/1RAlFPPEToevcKxUQ_Iu9AfoGIsz8FRQrSfJzfCj0ob0/edit?usp=sharing",
-        url: "https://docs.google.com/spreadsheets/d/1RAlFPPEToevcKxUQ_Iu9AfoGIsz8FRQrSfJzfCj0ob0/edit?usp=sharing",
-        description: "The bank is at your service! Go to the above link to access Disboard's Bank.",
+        title: "",
+        url: "",
+        description: "",
         timestamp: new Date(),
         footer: {
           text: ""
