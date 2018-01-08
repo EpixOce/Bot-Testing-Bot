14594ea1215827eae0813e4d94439b5d9186acf2
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-01-08 21:12:28 +1300

    Update event.js

--- commands/event.js
+++ commands/event.js
@@ -1,1 +1,14 @@
-
+exports.run = (client, message, args) => {
+    message.channel.send({embed:{
+        color: 3447003,
+        author: {
+            name: "Fidget Spinner Event! Prizes to be won!"
+        },
+        description: "**First Place:** YukiNeko - 82305\n**Second Place: n/a\n**Third Place: n/a",
+        timestamp: new Date(),
+        footer: {
+          text: ""
+        }
+      }
+    });
+}
