dc1c76610a90d399e01a5bed3072b9dc34ca7e1c
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-01-10 13:08:11 +1300

    Update event.js

--- commands/event.js
+++ commands/event.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
         author: {
             name: "Fidget Spinner Event! Prizes to be won!"
         },
-        description: "**First Place:** Planeswalker15 - 99460\n**Second Place:**JulianSynthetic - 98929\n**Third Place:** Snowwolf - 87873",
+        description: "**First Place:** Planeswalker15 - 99460\n**Second Place:** JulianSynthetic - 98929\n**Third Place:** ITotallyDontLikeFutaLolis - 97798",
         timestamp: new Date(),
         footer: {
           text: ""
