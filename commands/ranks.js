422ed3d1fcbd104110f5945810789bbfa79f394d
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-01-11 18:32:44 +1300

    Update ranks.js

--- commands/ranks.js
+++ commands/ranks.js
@@ -1,10 +1,10 @@
-exports.run = (client, message, args) => {
+oexports.run = (client, message, args) => {
     message.channel.send({embed:{
         color: 3447003,
         author: {
             name: "Mee6 Ranking"
         },
-        description: "**Ghost** - Lvl. 0\n**Imp** - Lvl. 5\n**Troll** - Lvl. 10\n**Ghoul** - Lvl. 20\n**Werewolf** - Lvl. 30\n**Vampire** - Lvl. 40\n**Vamp Lord** - Lvl. 50",
+        description: "**Ghost** - Lvl. 0\n**Imp** - Lvl. 5\n**Troll** - Lvl. 10\n**Werewolf** - Lvl. 20\n**Ghoul** - Lvl. 30\n**Vampire** - Lvl. 40\n**Vamp Lord** - Lvl. 50",
         timestamp: new Date(),
         footer: {
           text: ""
