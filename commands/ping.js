b6e6b4aed0066850343d7f71553f6b7803a52fbf
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-01 16:23:15 +1300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,18 +1,9 @@
 
 exports.run = (client, message, args) => {
-    message.channel.send({embed:{
-        color: 3447003,
-        author: {
-            name: "Pong!"
-        },
-        description: "Your ping is " + Math.ceil(client.ping, 1) + " ms." + " Please wait a few seconds before using this command again.",
-        timestamp: new Date(),
-        footer: {
-          text: ""
-        }
-      }
-    });
-}
+     const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    
+  }
 
         
 
