0318e83405d8adccb0f742a98838ec86c328009f
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-04 10:42:17 +1300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
-  message.channel.send(`**Pong!** Client's Ping is \`${client.pings[0]}ms\``);
+  message.reply(`**Pong!** Client's Ping is \`${client.pings[0]}ms\``);
 };
 
