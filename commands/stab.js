c4259a93fe85d25acba896ac933e4ea260eeb69a
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-04 12:30:10 +1300

    Update stab.js

--- commands/stab.js
+++ commands/stab.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
- message.channel.send(`${message.author.username}` + " stabs themselves in the foot.");
-}
+ message.channel.send(`${message.author.username}` + " stabs " + `${message.mention.username});
+};
