e626e9855441906ac48ea2d133ad4f6b21454cf0
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-04 10:22:58 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,17 +18,4 @@ client.on("message", message => {
   }
 });
 
-client.on("message", async message => {
-  if(message.author.bot) return;
-  
-  const args = message.content;
-  const command = args.shift().toLowerCase();
-  
-  if(command === "ping") {
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-});
-
-
 client.login(process.env.BOT_TOKEN);
