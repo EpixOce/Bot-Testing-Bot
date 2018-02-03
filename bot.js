168a700bb7ba86d5f20f410ca37025ddef1e384d
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-04 11:00:28 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
-client.on("message", message => {
-  if (message.author.bot) return;  
-  if(message.content.indexOf(config.prefix) !== 0) return;
-
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  fs.readdir("./events/", (err, files) => {
+fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     let eventFunction = require(`./events/${file}`);
@@ -18,6 +11,13 @@ client.on("message", message => {
     client.on(eventName, (...args) => eventFunction.run(client, ...args));
   });
 });
+
+client.on("message", message => {
+  if (message.author.bot) return;  
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
   try {
     let commandFile = require(`./commands/${command}.js`);
