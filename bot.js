781fb31b2f8d834506ed0755f461eb08b6cd8dc2
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-04 10:09:32 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,16 +6,18 @@ client.on("message", message => {
   if (message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
 
-
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-
 
   try {
     let commandFile = require(`./commands/${command}.js`);
     commandFile.run(client, message, args);
   } catch (err) {
     console.error(err);
+    
+    if (message.content.startsWith("welcome")) {
+    message.channel.send("Hello there! Welcome to **Deciduous!**");
+  }
   }
 });
 
