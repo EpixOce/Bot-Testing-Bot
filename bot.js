cf4f77b67db38669cbd5edddd426088b8f37a1ef
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-04 10:21:04 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("message", message => {
-  if (message.author.bot) return;
-  
-  if (message.content.startsWith("welcome")) {
-    message.channel.send("Hello there! Welcome to **Deciduous!**");
-  }
-  
+  if (message.author.bot) return;  
   if(message.content.indexOf(config.prefix) !== 0) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
@@ -20,6 +15,18 @@ client.on("message", message => {
   } catch (err) {
     console.error(err);
     
+  }
+});
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+  
+  const args = message.content;
+  const command = args.shift().toLowerCase();
+  
+  if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
 });
 
