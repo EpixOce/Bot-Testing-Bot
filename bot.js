f86eff98dae84274680485eb265888a48aa190a7
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-04 13:58:06 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,16 @@ const config = require("./config.json");
 client.on("message", message => {
   if (message.author.bot) return;  
   
+  const responseMsg = {
+    "yes": "https://cdn.discordapp.com/attachments/301400303893479434/409506457525157899/YES.png",
+    "hell yes": "https://cdn.discordapp.com/attachments/301400303893479434/409506479083880462/HELL_YES.png",
+    "no": "https://cdn.discordapp.com/attachments/301400303893479434/409506498700509194/NO.png"
+  };
+  
+  if(responseMsg[message.content]) {
+    message.channel.send(responseMsg[message.content]);
+  }
+  
   if(message.content.indexOf(config.prefix) !== 0) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
