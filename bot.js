b03ef2e9f5d42a60b06a5e9892bac7075306e46a
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-06 20:10:01 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,10 +8,7 @@ client.on("message", message => {
   if (message.author.bot) return;  
   
   const responseMsg = {
-    "YES": "https://cdn.discordapp.com/attachments/301400303893479434/409506457525157899/YES.png",
-    "HELL YES": "https://cdn.discordapp.com/attachments/301400303893479434/409506479083880462/HELL_YES.png",
-    "NO": "https://cdn.discordapp.com/attachments/301400303893479434/409506498700509194/NO.png",
-    "RULES": "**Welcome to Deciduous!**\nI hope you enjoy your stay. Before we get started, I'd like to lay out a few ground rules.\n \n:one: **Be wholesome.** Treat others with kindness and respect. Follow Discord's Community Guidelines found here: #welcome\n \n:two: **Keep conversations appropriate and inclusive.** General conversations in #tavern are conversations among multiple users. This means no topics meant to incite a negative response/argument. Move 1 on 1 discussions to DMs.\n \n:three: **Don't use selfbots.** No selfbot commands of any kind should be used on this server. This includes any program running on your account that is able to perform actions through the API.\n \n:four: **No NSFW.** Please don't link to, post images of, or allude to anything NSFW. If you have an inappropriate name or avatar, you will be asked to change it.\n \n:five: **No advertising.** Don't link to other servers here. Official Discord servers are fine. Asking for views on your video/stream/giveaway/website is also forbidden.\n \n:six: **Don't spam.** Don't spam ping other users. Do not under any circumstance ping someone and then delete it.\n \n:seven: **Admin have final verdict on rules.**  If a Admin asks you to stop doing something, you should stop.\n \nOnly mention **Admin** if a user is breaking the rules. **Include a reason / summary of what happened in your message.**"
+    "Do Not Disturb": "https://images-na.ssl-images-amazon.com/images/I/71dDwooliwL._SL1500_.jpg"    
   };
   
   if(responseMsg[message.content]) {
