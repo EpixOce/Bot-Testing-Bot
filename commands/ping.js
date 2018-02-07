97f40a4b2d70fb5c54b9be331d9a5c05c2c3d139
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-07 20:48:30 +1300

    Delete ping.js

--- commands/ping.js
+++
@@ -1,4 +0,0 @@
-exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const msg = await message.channel.send("Ping?");
-  msg.edit(`**Pong!** Your ping is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-};
