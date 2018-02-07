fdb58c6ca54bba2c3a006e7b2e9ae2d5c9284f3d
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-07 17:05:40 +1300

    Update sides.js

--- commands/sides.js
+++ commands/sides.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-    message.channel.send('Sides timer set!')
+    message.channel.send(`${message.author.username}` + ", **Sides Timer Set!**")
     setTimeout(function() {
         message.reply(" **Sides!** :carrot:");
     }, 300000)
