4e753bb5fc63e6df35799a769dc730fb5cff337f
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-07 17:16:12 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on("message", message => {
   if (message.author.bot) return;  
   
   const responseMsg = {
-    "#!adv": "`,adv`. The prefix in this server is `,`",
-    "#!stats": "`,stats`. The prefix in this server is `,`"
+    "#!adv": ":x: **Error!** The prefix in this server is `,`",
+    "#!stats": ":x: **Error!** The prefix in this server is `,`"
   };
   
   if(responseMsg[message.content]) {
