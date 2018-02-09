0c124c6c1aadc40f559053380f5d3ae1cb33a061
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-09 17:22:03 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,9 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
+  
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  
   client.user.setGame(`grinding DRPG`);
 });
 
