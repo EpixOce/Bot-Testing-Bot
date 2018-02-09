f0db76ce7cfa6376f3ea2a4676d7ef6fa69970d0
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-09 17:47:49 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {  
-  client.user.setPresence({game: 'Discord Dungeons!' });
+  client.user.setPresence({game: { name: 'Discord Dungeons!'}});
 });
 
 client.on("message", message => {
