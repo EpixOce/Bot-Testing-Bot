e276520229376668efff176184a8f4692d235b8c
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-11 18:32:34 +1300

    Update padv.js

--- commands/padv.js
+++ commands/padv.js
@@ -1,7 +1,7 @@
 
 exports.run = (client, message, args) => {
     setTimeout(function() {
-        message.channel.send(`${message.author.username}` + ", **Party Adventure!**, make sure it is your turn! :cherries:");
+        message.reply("**Party Adventure!**, make sure it is your turn! :cherries:");
     }, 19750)
 };
 
