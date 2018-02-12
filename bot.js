dc98925af3d4017b9887be0ae33f5740158663d8
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-12 18:34:43 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,8 @@ client.on("message", message => {
     "#!chop": ":deciduous_tree: " + `${message.author.username}` + " went to chop some wood but fell into a pithall.",
     "#!mine": ":pick: " + `${message.author.username}` + " found a Cinnarbar, got poisoned and died.",
     "#!forage": ":tulip: " + `${message.author.username}` + " went foraging and got stung by bees.",
-    "#!fish": ":fishing_pole_and_fish: " + `${message.author.username}` + " caught a shark and got their head bitten off."
+    "#!fish": ":fishing_pole_and_fish: " + `${message.author.username}` + " caught a shark and got their head bitten off.",
+    "#!search": "You search the location, and found a toxic apple, but since you're not that smart, you ate it. - Pato Deep Web, and his best wishes for your afterlife."
   };
   
   if(responseMsg[message.content]) {
