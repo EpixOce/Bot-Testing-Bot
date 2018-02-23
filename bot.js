f55907f206ca873b122072de92524d8778f004ea
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-23 16:51:51 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on("message", message => {
     "#!catch": `${message.author.username}` + " tried to catch the insanely agile Epix, but failed and sprained his ankles in the process."
   };
   
-  if(reply.message.content.shift().toLowercase()) {
+  if(reply[message.content.shift().toLowercase()]) {
     message.channel.send(reply[message.content]);
   };
   
