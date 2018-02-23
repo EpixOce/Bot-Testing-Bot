6b731aefb9a79df07644aaa1d816b0e1b1073366
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-23 16:14:53 +1300

    Update chop.js

--- cmdreplies/chop.js
+++ cmdreplies/chop.js
@@ -1,1 +1,4 @@
-a
+exports.run = (client, message, args) => {
+    message.channel.send(":deciduous_tree: " + `${message.author.username}` + " went to chop some wood but fell into a pithall.")
+};
+
