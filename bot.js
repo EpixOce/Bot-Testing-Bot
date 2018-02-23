7ad79f6471105c4d4165d6e79696bb205684dfa5
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-02-23 16:20:51 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,10 +22,10 @@ client.on("message", message => {
   };
   
   if(message.content.indexOf(config.prefix) !== 0) return;
-  if(message.content.indexOf(config.prefix2) !== 0) return;
+  if(message.content.indexOf(config.prefixtwo) !== 0) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const cmdargs = message.content.slice(config.prefix2.length).trim().split(/ +/g);
+  const cmdargs = message.content.slice(config.prefixtwo.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
   try {
