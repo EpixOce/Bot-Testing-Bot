3e3a79cc4c1159eccd1453999233e456af53005b
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-03-06 17:21:49 +1300

    Update adv.js

--- commands/adv.js
+++ commands/adv.js
@@ -1,4 +1,6 @@
 exports.run = (client, message, args) => {
+    if (message.author.id !== "196203663054733313") return;
+    
     setTimeout(function() {
         message.reply("**Adventure!** :arrow_down:");
     }, 13900)
