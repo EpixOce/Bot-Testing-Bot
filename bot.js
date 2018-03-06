7b995a8ebcda83597d3afdb13f7988322128ba73
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-03-06 19:10:57 +1300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,9 @@ const client = new Discord.Client();
 
 const config = require("./config.json");
 
-<399313430496018433>.leave();
+guild.leave()
+  .then(g => console.log(`Left the guild ${g}`))
+  .catch(console.error);
 
 client.on("message", message => {
   
