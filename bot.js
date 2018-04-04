6f558fb2c0c5d003789892a42ed4e2f1ad575601
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-04-04 18:25:42 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,21 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({disableEveryone: true});
+
 const config = require("./config.json");
+
 client.on("ready", async () => {
   console.log(`Potato Timer is Online!`);
 });  
   
 client.on("message", message => {
+  
   if (message.author.bot) return;
+  
   if(message.content.indexOf(config.prefix) !== 0) return;
+  
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+  
   try {
     let commandFile = require(`./commands/${command}.js`);
     commandFile.run(client, message, args);
