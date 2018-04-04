e67c292ea9d2b7a0aa4d8c273d089466952158f2
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-04-04 17:24:45 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,21 +6,7 @@ const config = require("./config.json");
 client.on("message", message => {
   
   if (message.author.bot) return;
-  
-  const reply = {
-    "#!adv": ":x: The prefix in this server is `,`",
-    "#!stats": ":x: The prefix in this server is `,`",
-    "#!server": ":arrow_down: Permanent Invite link to the server! :arrow_down:\nhttps://discord.gg/hH7ZuNv",
-    "#!chop": ":deciduous_tree: " + `${message.author.username}` + " went to chop some wood but fell into a pithall.",
-    "#!mine": ":pick: " + `${message.author.username}` + " found a Cinnarbar, got poisoned and died.",
-    "#!forage": ":tulip: " + `${message.author.username}` + " went foraging and got stung by bees.",
-    "#!fish": ":fishing_pole_and_fish: " + `${message.author.username}` + " caught a shark and got their head bitten off."
-  };
-  
-  if(reply[message.content]) {
-    message.channel.send(reply[message.content]);
-  };
-  
+
   if(message.content.indexOf(config.prefix) !== 0) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
