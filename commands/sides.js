46b3a35b9653c4e0705e72cf4c1cc1d7a3107944
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-04-04 20:21:19 +1200

    Delete sides.js

--- commands/sides.js
+++
@@ -1,7 +0,0 @@
-exports.run = (client, message, args) => {
-    message.channel.send(`${message.author.username}` + ", **Sides Timer Set!**")
-    setTimeout(function() {
-        message.reply(" **Sides!** :carrot:");
-    }, 300000)
-};
-
