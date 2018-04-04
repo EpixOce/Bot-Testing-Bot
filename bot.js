8f6858f89d8be1455c2a634d556dc3788a4f16bb
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-04-04 20:20:09 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,38 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({disableEveryone: true});
-
 const config = require("./config.json");
+const Discord = require("discord.js");
 
-client.on("ready", async () => {
-  console.log(`Potato Timer is Online!`);
-});  
-  
-client.on("message", message => {
-  
-  if (message.author.bot) return;
-  
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  try {
-    let commandFile = require(`./commands/${command}.js`);
-    commandFile.run(client, message, args);
-  } catch (err) {
-    console.error(err);
-  }  
+const bot = new Discord.Client({disableEveryone: true})
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is up and running!`)
+    bot.user.setGame("DiscordRPG!")
 });
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+  
+  if(cmd === `${prefix}adv`) {
+    setTimeout(function() {
+      message.reply("**Adventure!** :arrow_down:");
+    }, 13900)
+  };
+  if(cmd === `${prefix}padv`) {
+    setTimeout(function() {
+        message.reply("**Party Adventure!**, make sure it is your turn! :cherries:");
+    }, 19750)
+  };
+  if(cmd === `${prefix}sides`) {
+    message.channel.send(`${message.author.username}` + ", **Sides Timer Set!**")
+    setTimeout(function() {
+        message.reply(" **Sides!** :carrot:");
+    }, 300000)
+  }
+});
+
 client.login(process.env.POTATO_TOKEN);
