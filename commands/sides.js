8e3ff8aff770799f89f8a9c654785bc52d72bb89
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-04-20 08:39:53 +1200

    Update sides.js

--- commands/sides.js
+++ commands/sides.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send(`${message.author.username}` + ", **Sides Timer Set!**")
+    const m = await message.channel.send(`${message.author.username}` + ", **Sides Timer Set!**");
+    m.delete()
         setTimeout(function() {
             message.reply(" **Sides!** :carrot:");
         }, 300000)
