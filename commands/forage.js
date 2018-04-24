cfca773bd7c189b8cc2872acc604f41da70ac6be
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-04-24 20:33:34 +1200

    Update forage.js

--- commands/forage.js
+++ commands/forage.js
@@ -3,14 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     setTimeout(function() {
         message.delete();
-    }, 10000);
-    setTimeout(async function() {
-        const r = await message.reply("**Time to Forage!**");
-        setTimeout(function() {
-            r.delete();
-        }, 20000);
-    }, 300000)
+    }, 10000)
 }
+
 module.exports.help = {
     name: "forage"
 }
