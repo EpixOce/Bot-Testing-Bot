ed4768195a6dfa244c0f953af306fe925e783f31
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-04-24 17:13:20 +1200

    Update fish.js

--- commands/fish.js
+++ commands/fish.js
@@ -3,9 +3,14 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     setTimeout(function() {
         message.delete();
-    }, 10000)
+    }, 10000);
+    setTimeout(async function() {
+        const r = await message.reply(" Time to **Fish!**");
+        setTimeout(function() {
+            r.delete();
+        }, 30000);
+    }, 300000)
 }
-
 module.exports.help = {
     name: "fish"
 }
